5301de36642906315c9a74f775f8c043ac3e5b94
martinnss <martin.olivarest@utem.cl>
2023-02-03 00:54:45 -0300

    js

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ menuEmail.addEventListener('click',toggleDesktopMenu)
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
 }
+
+/* 1. traer elemento a interactuar y el nuevo 2. funcion que activa el elemento "nuevo" */
+
+const burgerMenu = document.querySelector('.menu')
+const burgerMenuInside = document.querySelector('.mobile-menu')
+
+burgerMenu.addEventListener('click',toggleBurgerMenu)
+
+function toggleBurgerMenu (){
+    burgerMenuInside.classList.toggle('inactive')
+}
